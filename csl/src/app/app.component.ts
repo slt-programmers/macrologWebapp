5facf9c2bbef4e112971ca9ea512d3a7ec50486d
Carmen Scholte Lubberink <Carmen@BTO.(none)>
2018-06-27 19:12:28 +0200

    Navbar animatie

--- csl/src/app/app.component.ts
+++ csl/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,13 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
+  @ViewChild('navbar') private navbarElement: ElementRef;
+
   public title: string;
+	private navbar;
 	public navToggle: boolean = false;
 
   constructor() {}
 
   ngOnInit() {
     this.title = 'Macrolog Webapp';
+		this.navbar = this.navbarElement.nativeElement;
+		console.log(this.navbar);
+		console.log(this.navbarElement);
+		this.approot = this.navbar.parentElement;
+		this.body = this.approot.parentElement;
+		console.log(this.approot);
+		console.log(this.body);
   }
+
+
+	// Navigation
+	openNav() {
+		this.navbar.style.marginLeft = '0';
+		this.navbar.style.width = '100%';
+	}
+
+	closeNav() {
+		this.navbar.style.marginLeft = '-300px';
+		this.navbar.style.width = 'auto';
+	}
 
 }
