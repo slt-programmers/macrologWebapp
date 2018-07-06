a20f33ba93ef9691173007ebe37ed3594ffc9ef7
Arjan Tienkamp <arjan.tienkamp@gmail.com>
2018-07-06 21:37:34 +0200

    opschonen

--- csl/src/app/model/food.ts
+++ csl/src/app/model/food.ts
@@ -11,17 +11,3 @@ export class Food {
   public portions: Portion[];
 	constructor() {}
 }
-/*
-export class Food {
-	constructor(
-		public name: string,
-		public unit: number,
-		public unitName: String,
-		public optionalGrams: number,
-		public protein: number,
-		public fat: number,
-		public carbs: number,
-	) {}
-
-}
-*/
