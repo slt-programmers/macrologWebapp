a8f87be1ee7eaa77172745546fc3095611b347c0
Carmen Scholte Lubberink <Carmen@BTO.(none)>
2018-07-06 10:36:13 +0200

    SCSS public styling geordend, modal add-food aangepast

--- csl/src/app/components/add-food-modal/add-food-modal.component.ts
+++ csl/src/app/components/add-food-modal/add-food-modal.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
+class Portion {
+	constructor() {
+		public amount: string = '';
+		public description: string = '';
+	}
+}
+
 @Component({
   selector: 'add-food-modal',
   templateUrl: './add-food-modal.component.html'
@@ -9,7 +16,12 @@ export class AddFoodModalComponent implements OnInit {
 	@Output() close: EventEmitter<boolean> = new EventEmitter<boolean>();
 
 	public title = 'Add food to the database';
+	public food = '';
 	public nutrients = 'grams';
+	public unitName = '';
+	public grams;
+
+	public portions = [];
 
   constructor() { }
 
@@ -18,6 +30,20 @@ export class AddFoodModalComponent implements OnInit {
 
 	closeModal() {
 		this.close.emit(true);
+	}
+
+	onNext() {
+		this.currentStep = 'step2';
+	}
+
+	newPortion() {
+		this.portions.push(new Portion());
+	}
+
+	removePortion(index) {
+		// start removing from index
+		// remove 1 item
+		this.portions.splice(index, 1);
 	}
 
 }
