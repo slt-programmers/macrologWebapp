5a9a6cf764e4085e3b22367b888e28ae0fc5d6f9
Carmen Scholte Lubberink <Carmen@BTO.(none)>
2018-07-25 20:00:46 +0200

    Styling app component verplaatst

--- csl/src/app/app.component.ts
+++ csl/src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
 
