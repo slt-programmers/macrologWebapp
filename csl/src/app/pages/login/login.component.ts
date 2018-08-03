d2308ea9215c6acdf34ca3c8d3cbacc251d39fe2
Carmen Scholte Lubberink <Carmen@BTO.(none)>
2018-08-03 08:51:34 +0200

    Login refactor

--- csl/src/app/pages/login/login.component.ts
+++ csl/src/app/pages/login/login.component.ts
@@ -1,64 +1,47 @@
-
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
 
 
 import { AuthenticationService } from '../../services/auth.service';
-@Component({templateUrl: 'login.component.html'})
+@Component({
+	templateUrl: 'login.component.html'
+})
 export class LoginComponent implements OnInit {
-    loginForm: FormGroup;
-    loading = false;
-    submitted = false;
-    returnUrl: string;
-    error = '';
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private route: ActivatedRoute,
-        private router: Router,
-        private authenticationService: AuthenticationService) {}
+  private returnUrl: string;
+  public error = '';
+	public username: string;
+	public password: string;
 
-    ngOnInit() {
-        this.loginForm = this.formBuilder.group({
-            username: ['', Validators.required],
-            password: ['', Validators.required]
-        });
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService) {}
 
-        // reset login status
-        this.authenticationService.logout();
+  ngOnInit() {
+    // reset login status
+    this.authenticationService.logout();
 
-        // get return url from route parameters or default to '/'
-        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-    }
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
 
-    // convenience getter for easy access to form fields
-    get f() { return this.loginForm.controls; }
-
-    onSubmit() {
-        this.submitted = true;
-
-        // stop here if form is invalid
-        if (this.loginForm.invalid) {
-            return;
-        }
-
-        this.loading = true;
-        this.authenticationService.login(this.f.username.value, this.f.password.value)
-            .pipe(first())
-            .subscribe(
-                data => {
-                    this.router.navigate([this.returnUrl]);
-                },
-                error => {
-                    console.log(error);
-                    if (error.status === 401){
-                      this.error = "Invalid username/password"
-                    } else {
-                      this.error = "Unknown error:" + error.message;
-                    }
-                    this.loading = false;
-                });
-    }
+  onSubmit() {
+		this.error = '';
+    this.authenticationService.login(this.username, this.password)
+        .pipe(first())
+        .subscribe(
+            data => {
+                this.router.navigate([this.returnUrl]);
+            },
+            error => {
+                console.log(error);
+                if (error.status === 401){
+                  this.error = 'Invalid username/password';
+                } else {
+                  this.error = 'Unknown error:' + error.message;
+                }
+            });
+  }
 }
