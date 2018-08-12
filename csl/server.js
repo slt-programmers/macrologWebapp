9e5fce71a9e575eaf56f4ed215644ef9be993954
Arjan Tienkamp <arjan.tienkamp@gmail.com>
2018-08-12 10:33:26 +0200

    heroku more

--- csl/server.js
+++ csl/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/macrolog'));
+app.use(express.static(__dirname + '/dist/csl'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/macrolog/index.html'));
+res.sendFile(path.join(__dirname+'/dist/csl/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
