c0281f3712838f6de66aa90b9854abea7b2a9097
Arjan Tienkamp <arjan.tienkamp@gmail.com>
2018-08-15 12:22:15 +0200

    force https on deployment

--- csl/server.js
+++ csl/server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+app.use(function(req, res, next) {
+      if ((req.get('X-Forwarded-Proto') !== 'https')) {
+        res.redirect('https://' + req.get('Host') + req.url);
+      } else
+        next();
+    });
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/'));
@@ -11,6 +17,7 @@ app.get('/*', function(req,res) {
 
 res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
