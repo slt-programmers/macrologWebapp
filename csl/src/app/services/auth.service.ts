54978f80333f776420e7501112ec80b3df588209
Carmen Scholte Lubberink <Carmen@BTO.(none)>
2018-09-21 09:51:25 +0200

    Uitlijning

--- csl/src/app/services/auth.service.ts
+++ csl/src/app/services/auth.service.ts
@@ -6,7 +6,7 @@ import { environment } from '../../environments/environment';
 @Injectable({ providedIn: 'root' })
 export class AuthenticationService {
 
-    macrologBackendUrl = '//'+environment.backend+'/api';
+	macrologBackendUrl = '//'+environment.backend+'/api';
 
   constructor(private http: HttpClient) {}
 
@@ -19,20 +19,21 @@ export class AuthenticationService {
 		return (user != null);
 	}
 
-    login(username: string, password: string) {
-        return this.http.post<any>(this.macrologBackendUrl + '/authenticate', { username: username, password: password })
-            .pipe(map((res:any) => {
-                if (res && res.token) {
-                   localStorage.setItem('currentUser', JSON.stringify({ 'user':res.name, 'token': res.token }));
-                }
-            }));
-    }
+	login(username: string, password: string) {
+    return this.http.post<any>(this.macrologBackendUrl + '/authenticate', { username: username, password: password })
+        .pipe(map((res:any) => {
+          if (res && res.token) {
+            localStorage.setItem('currentUser', JSON.stringify({ 'user':res.name, 'token': res.token }));
+					}
+				})
+		);
+	}
 
-    logout() {
-       setTimeout(() => {
-          localStorage.removeItem('currentUser');
-       });
-    }
+	logout() {
+		setTimeout(() => {
+       localStorage.removeItem('currentUser');
+    });
+	}
 
 	signup(username: string, password: string, email: string) {
 		return this.http.post(this.macrologBackendUrl + '/signup', { username: username, password: password, email: email });
