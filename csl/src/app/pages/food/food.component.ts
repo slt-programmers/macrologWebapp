362e4a2daacea9802b2b73a574e8a3334111310a
Carmen Scholte Lubberink <Carmen@BTO.(none)>
2018-09-23 08:27:09 +0200

    Search functie voor food pagina

--- csl/src/app/pages/food/food.component.ts
+++ csl/src/app/pages/food/food.component.ts
@@ -1,5 +1,6 @@
-import{Component, OnInit}from '@angular/core';
+import {Component, OnInit}from '@angular/core';
 import {FoodService}from '../../services/food.service';
+import {Food} from '../../model/food';
 
 @Component({
   selector: 'app-food',
@@ -7,12 +8,14 @@ import {FoodService}from '../../services/food.service';
 })
 export class FoodComponent implements OnInit {
 
-  public foodResult = new Array();
-	public pagedFood = new Array();
+  private foodResult = new Array();;
+	public pagedFood = new Array<Food>();
+	public searchableFood = new Array<Food>();
 	public modalIsVisible: boolean = false;
 	public currentPage = 1;
 	public itemsPerPage = 15;
   public selectedFood = null; // input voor modal popup
+	public searchInput = '';
 
   constructor(private foodService: FoodService) { }
 
@@ -22,17 +25,34 @@ export class FoodComponent implements OnInit {
 
 	private loadAllFood(){
     this.foodService.getAllFood().subscribe(
-      data => { this.foodResult = data;
+      data => {
+        this.foodResult = data;
+				this.searchableFood = this.foodResult;
         this.getPagedFood(this.currentPage);
       },
 			error => console.log(error)
 		);
   }
 
-	getPagedFood(page: number) {
-		this.pagedFood = this.foodResult.slice(
+	getPagedFood(page: number): void {
+		this.pagedFood = this.searchableFood.slice(
 			(page * this.itemsPerPage) - this.itemsPerPage,
 			((page + 1) * this.itemsPerPage) - this.itemsPerPage);
+	}
+
+	public findFoodMatch(): void {
+		console.log(this.searchInput);
+		let foodMatch = new Array<Food>();
+		for (let food of this.foodResult) {
+      let matchFoodName = food.name.toLowerCase().indexOf(this.searchInput.toLowerCase()) >= 0;
+			if (matchFoodName) {
+				foodMatch.push(food);
+			}
+		}
+
+		this.currentPage = 1;
+		this.searchableFood = foodMatch;
+		this.getPagedFood(this.currentPage);
 	}
 
 	public openModal(food) {
@@ -44,5 +64,13 @@ export class FoodComponent implements OnInit {
     this.loadAllFood();
 		this.modalIsVisible = false;
 	}
+
+	public clearSearch(): void {
+		this.searchInput = '';
+		this.searchableFood = this.foodResult;
+		this.currentPage = 1;
+		this.getPagedFood(this.currentPage);
+	}
+
 
 }
