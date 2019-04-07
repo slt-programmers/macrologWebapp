6206259533ef84c34ac71bbcefc78eb1bcf9cbea
Carmen Scholte Lubberink <cscholtelubberink@newnexusmobile.nl>
2019-04-07 11:14:43 +0200

    Portion unit removed

--- csl/src/app/model/portion.ts
+++ csl/src/app/model/portion.ts
@@ -2,13 +2,11 @@ export class Portion {
 
 	public id: number;
 	public grams: number;
-	public unit: number;
 	public description: string;
 	public macros;
 
 	constructor () {
 		this.grams = 0;
-		this.unit = 0;
 		this.description = null;
 		this.macros = {'protein': 0, 'fat': 0, 'carbs': 0};
 	}
