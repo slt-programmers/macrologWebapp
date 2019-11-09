24055dfce2dbeb8c4aaf1272c409a2f9d940d29c
Arjan Tienkamp <arjan.tienkamp@gmail.com>
2019-11-09 19:39:46 +0100

    same build fix for this branch as in the about branch

--- slt/src/app/pages/guest/login/login.component.ts
+++ slt/src/app/pages/guest/login/login.component.ts
@@ -33,6 +33,8 @@ export class LoginComponent implements OnInit {
 	public forgotError: string;
 
 	public showForgotPwModal = false;
+	public menuToggle = false;
+
 
 	constructor(
 		private route: ActivatedRoute,
@@ -101,4 +103,8 @@ export class LoginComponent implements OnInit {
 					this.forgotError = 'The email adress was not found';
 				});
 	}
+
+	public toggleMenu() {
+		this.menuToggle = !this.menuToggle;
+	}	
 }
