ad8038d06c7a0e67e3da3aca4fe784e8358944f3
DESKTOP-E7N1CAB\Carmen <c.scholte.lubberink@gmail.com>
2021-10-09 12:59:34 +0200

    Restyle about page

--- src/app/pages/guest/about/about.component.ts
+++ src/app/pages/guest/about/about.component.ts
@@ -1,17 +1,8 @@
 import { Component} from '@angular/core';
-import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
 	selector: 'about',
 	templateUrl: './about.component.html',
-  animations: [
-		trigger('enterLeaveTrigger', [
-			transition(':enter', [
-				style({ opacity: 0, marginTop: '15px' }),
-				animate('0.3s', style({ opacity: 1, marginTop: '0' })),
-			]),
-		]),
-	],
 	styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
