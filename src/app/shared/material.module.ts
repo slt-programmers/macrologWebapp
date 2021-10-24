7a17065ef8d5e4c37a1de684fa2ca9c92e8eaaec
DESKTOP-E7N1CAB\Carmen <c.scholte.lubberink@gmail.com>
2021-10-24 11:48:57 +0200

    Remove radio material

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from "@angular/core";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
-import { MatRadioModule } from "@angular/material/radio";
 
 @NgModule({
   imports: [
     MatInputModule,
     MatFormFieldModule,
-    MatRadioModule,
   ], 
   exports: [
     MatInputModule,
     MatFormFieldModule,
-    MatRadioModule,
   ]
 })
 export class MaterialModule { }
