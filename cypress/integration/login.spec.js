ae056ca88ab3cb81058bdd30c6eef87a8d14dfd0
DESKTOP-E7N1CAB\Carmen <c.scholte.lubberink@gmail.com>
2021-11-16 07:30:57 +0100

    ML-87 Add register flow for e2e testing

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe('Login', () => {
-  
+
   beforeEach(() => {
     const backendUrl = Cypress.env('backendUrl');
 
@@ -9,6 +9,9 @@ describe('Login', () => {
     cy.intercept('POST', `${backendUrl}/api/authenticate`, (req) => {
       req.reply({ statusCode: 200, fixture: 'authenticate.json' })
     }).as('authenticate');
+    cy.intercept('POST', `${backendUrl}/api/signup`, (req) => {
+      req.reply({ statusCode: 200, fixture: 'register.json' })
+    }).as('register');
 
     cy.visit('localhost:4200');
   });
@@ -22,6 +25,20 @@ describe('Login', () => {
     cy.get('#password').type('thisIsSecret');
     cy.get('#loginButton').click();
     cy.get('#menuDiary').should('be.visible');
+  });
+
+  it('should register user', () => {
+    cy.get('#menuRegister').click();
+    cy.get('#registerButton').click();
+    cy.get('#usernameRegisterRequired').should('be.visible');
+    cy.get('#emailRequired').should('be.visible');
+    cy.get('#passwordRegisterRequired').should('be.visible');
+    cy.get('#usernameRegister').type('username');
+    cy.get('#email').type('email@email.com');
+    cy.get('#passwordRegister').type('thisIsSecret');
+    cy.get('#registerButton').click();
+    cy.get('#menuDiary').should('be.visible');
+    cy.get('#onboardingTitle').should('be.visible');
   });
 
 });
